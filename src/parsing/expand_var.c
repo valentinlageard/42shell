6660b5955b927acbe9bd00a07777828cdf3ad8f8
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-01-16 14:06:34 +0100

    nonexistent variable managed

--- src/parsing/expand_var.c
+++ src/parsing/expand_var.c
@@ -6,7 +6,7 @@
 /*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 18:53:23 by valentin          #+#    #+#             */
-/*   Updated: 2021/01/16 13:50:37 by valentin         ###   ########.fr       */
+/*   Updated: 2021/01/16 14:03:32 by valentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ void	expand_var(t_tok *tok, int *i, t_shell *shell)
 	while ((ft_isalnum(tok->str[j])) || tok->str[j] == '_')
 		j++;
 	slice = ft_strslice(tok->str, *i + 1, j);
-	if ((val = get_envval(slice, shell->env)))
-	{
-		nstr_size = *i + ft_strlen(val) + (ft_strlen(tok->str) - j) + 1;
-		nstr = (char *)malloc(sizeof(char) * nstr_size); // PROTECT
-		ft_memset(nstr, '\0', nstr_size);
-		ft_memcpy(nstr, tok->str, *i);
-		ft_memcpy(&(nstr[*i]), val, ft_strlen(val));
-		ft_memcpy(&(nstr[*i + ft_strlen(val)]), &(tok->str[j]),
-			ft_strlen(tok->str) - j);
-		free(tok->str);
-		tok->str = nstr;
-	}
+	val = get_envval(slice, shell->env);
+	if (!val)
+		val = "";
+	nstr_size = *i + ft_strlen(val) + (ft_strlen(tok->str) - j) + 1;
+	nstr = (char *)malloc(sizeof(char) * nstr_size); // PROTECT
+	ft_memset(nstr, '\0', nstr_size);
+	ft_memcpy(nstr, tok->str, *i);
+	ft_memcpy(&(nstr[*i]), val, ft_strlen(val));
+	ft_memcpy(&(nstr[*i + ft_strlen(val)]), &(tok->str[j]),
+		ft_strlen(tok->str) - j);
+	free(tok->str);
+	tok->str = nstr;
 	*i = j;
 	free(slice);
 }
@@ -46,17 +46,13 @@ void	expand_vars(t_tok *ltok, t_shell *shell)
 	t_tok	*tmp;
 
 	tmp = ltok;
-	// Pour chaque token
 	while (tmp)
 	{
-		// Si le token est un texte 0 ou un double quote 2
 		if (tmp->type == 0 || tmp->type == 2)
 		{
 			i = 0;
-			// Pour chaque caractère
 			while (tmp->str[i])
 			{
-				// Si le caractère est un $
 				if (tmp->str[i] == '$')
 					expand_var(tmp, &i, shell);
 				i++;
