16259bf28be04f3924fb445638e7d0842af04efd
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-02-10 14:37:33 +0100

    explicit kill

--- src/utils/signal_handling.c
+++ src/utils/signal_handling.c
@@ -6,18 +6,33 @@
 /*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 14:47:02 by valentin          #+#    #+#             */
-/*   Updated: 2021/02/09 20:50:50 by valentin         ###   ########.fr       */
+/*   Updated: 2021/02/10 14:33:00 by valentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	kill_children(int sig, t_shell *shell)
+{
+	t_lpid	*tmp;
+
+	tmp = shell->lpids;
+	while (tmp)
+	{
+		kill(tmp->pid, sig);
+		tmp = tmp->next;
+	}
+}
 
 void	handle_signals(int signum)
 {
 	if (signum == SIGQUIT)
 	{
 		if (g_shell->is_executing)
+		{
+			kill_children(SIGQUIT, g_shell);
 			write(2, "Quit (core dumped)\n", 19);
+		}
 		else
 			write(2, "\b\b  \b\b", 6);
 	}
@@ -25,6 +40,8 @@ void	handle_signals(int signum)
 	{
 		if (!(g_shell->is_executing))
 			reinit_read_state(&(g_shell->read_state));
+		else
+			kill_children(SIGINT, g_shell);
 		g_shell->pass = 1;
 		g_shell->exit_code = 128 + SIGINT;
 		write(2, "\n", 1);
